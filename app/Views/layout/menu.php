79da7e9956624ebc06e8841e75a44871cf702af7
Juan Carlos Pedroso <kyanuj@gmail.com>
2023-07-07 08:14:20 -0300

    Mejorando plantillas

---
+++ app/Views/layout/menu.php
@@ -0,0 +1,16 @@
+      <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+          <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+          <img src="/assets/images/logo/logo.png" alt="" width="65" height="65">
+          </a>
+
+          <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+            <li><a href="/" class="nav-link px-2 link-secondary">INICIO</a></li>
+            <li><a href="/about" class="nav-link px-2 link-dark">+INFO</a></li>
+            <li><a href="/contact" class="nav-link px-2 link-dark">CONTACTO</a></li>
+            <li><a href="/products" class="nav-link px-2 link-dark">PRODUCTOS</a></li>
+          </ul>
+
+          <div class="col-md-3 text-end">
+                <a href="/login" class="btn btn-outline-primary me-2">INICIAR SESION</a>
+        </div>
+      </header>
