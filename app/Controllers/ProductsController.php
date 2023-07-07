dcad8c7e33ecc6a373e04f5a045f83d521ff1037
Juan Carlos Pedroso <kyanuj@gmail.com>
2023-07-07 07:34:03 -0300

    Creando controlador y vista para productos

---
+++ app/Controllers/ProductsController.php
@@ -0,0 +1,12 @@
+<?php
+// app/Controllers/ProductsController.php
+
+class ProductsController
+{
+
+    public function index()
+    {
+        require_once __DIR__ . '/../Views/Products/index.php';
+    }
+
+}
